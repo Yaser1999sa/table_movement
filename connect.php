95eda06f9c0903054729e92349715c46d1348825
Yaser1999sa <85839835+Yaser1999sa@users.noreply.github.com>
2021-07-05 20:27:48 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$username = "root";
+$password = "";
+$datebase = new PDO("mysql:host=localhost; dbname=robot_move;",$username
+,$password);
+
+if($database){
+echo'its connected';
+}
+?>
---
+++ table_movement.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 05 يوليو 2021 الساعة 18:47
+-- إصدار الخادم: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robot_move`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `table_movement`
+--
+
+CREATE TABLE `table_movement` (
+  `forward` varchar(11) NOT NULL,
+  `left` varchar(11) NOT NULL,
+  `stop` varchar(11) NOT NULL,
+  `right` varchar(11) NOT NULL,
+  `backward` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
